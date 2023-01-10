f750a00cf1474044e79f705c2483f5664036d5a4
Bryan Parada <bryan.parada.cea@gmail.com>
2023-01-10 17:59:30 -0300

    FM17 - Header diagonal

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -25,3 +25,52 @@ class HeaderBorderRadius extends StatelessWidget {
     );
   }
 }
+
+class HeaderDiagonal extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      //color: Color(0xff615AAB),
+      child: CustomPaint(
+        painter: _HeaderDiagonalPainter(),
+      )
+    );
+  }
+}
+
+class _HeaderDiagonalPainter extends CustomPainter{
+  @override
+  void paint(Canvas canvas, Size size) { 
+
+    final pencil = new Paint();
+
+    //Propiedades lapiz
+    pencil.color = Color(0xff615AAB);
+    pencil.style = PaintingStyle.fill; //fill para rellenar - stroke para dibujar
+    pencil.strokeWidth = 2;
+
+    final path = new Path();
+
+    //dibujar con el path y el lapiz
+    path.moveTo(0, size.height * 0.35);
+    path.lineTo(size.width, size.height * 0.30);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+    path.lineTo(0, size.height * 0.5); //no es necesaria
+
+
+    canvas.drawPath(path, pencil);
+
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+   return true;
+  }
+
+
+
+}
