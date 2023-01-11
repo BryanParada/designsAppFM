8c4cce0164ad5ad582d5cafd548b537d415a1cd4
Bryan Parada <bryan.parada.cea@gmail.com>
2023-01-11 15:28:28 -0300

    FM21 - HeaderWaves

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -214,3 +214,49 @@ class _HeaderCurvePainter extends CustomPainter{
   }
 
 }
+
+
+class HeaderWave extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      //color: Color(0xff615AAB),
+      child: CustomPaint(
+        painter: _HeaderWavePainter(),
+      )
+    );
+  }
+}
+
+class _HeaderWavePainter extends CustomPainter{
+  @override
+  void paint(Canvas canvas, Size size) { 
+
+    final pencil = new Paint();
+
+    //Propiedades lapiz
+    pencil.color = Color(0xff615AAB);
+    pencil.style = PaintingStyle.fill; //fill para rellenar - stroke para dibujar
+    pencil.strokeWidth = 15;
+
+    final path = new Path();
+
+    //dibujar con el path y el lapiz 
+    path.lineTo(0, size.height * 0.25);  
+    path.quadraticBezierTo(size.width * 0.25, size.height * 0.30, size.width * 0.5, size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.75, size.height * 0.20, size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);  
+
+    canvas.drawPath(path, pencil);
+
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+   return true;
+  }
+
+}
