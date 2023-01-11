e140864586d621f0a395049bdf21a7d2fd7f0c5f
Bryan Parada <bryan.parada.cea@gmail.com>
2023-01-11 15:00:33 -0300

    FM19 - Header peak

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -124,3 +124,49 @@ class _HeaderTrianglePainter extends CustomPainter{
   }
 
 }
+
+
+class HeaderPeak extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      //color: Color(0xff615AAB),
+      child: CustomPaint(
+        painter: _HeaderPeakPainter(),
+      )
+    );
+  }
+}
+
+class _HeaderPeakPainter extends CustomPainter{
+  @override
+  void paint(Canvas canvas, Size size) { 
+
+    final pencil = new Paint();
+
+    //Propiedades lapiz
+    pencil.color = Color(0xff615AAB);
+    pencil.style = PaintingStyle.fill; //fill para rellenar - stroke para dibujar
+    pencil.strokeWidth = 15;
+
+    final path = new Path();
+
+    //dibujar con el path y el lapiz 
+    path.lineTo(0, size.height * 0.25); 
+    path.lineTo(size.width * 0.5, size.height * 0.30); 
+    path.lineTo(size.width, size.height * 0.25); 
+    path.lineTo(size.width,0); 
+
+    canvas.drawPath(path, pencil);
+
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+   return true;
+  }
+
+}
