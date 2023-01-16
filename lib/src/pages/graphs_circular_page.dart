3e737db0959374a0a67540ccd32cf89521eebb94
Bryan Parada <bryan.parada.cea@gmail.com>
2023-01-16 16:46:35 -0300

    FM53 - multiple RadialProgress

--- lib/src/pages/graphs_circular_page.dart
+++ lib/src/pages/graphs_circular_page.dart
@@ -28,20 +28,56 @@ class _GraphCircularPageState extends State<GraphCircularPage> {
          
         },
       ),
-      body: Center(
-          child: Container(
-            width: 300,
-            height: 300,
-            //color: Colors.red,
-            child: RadialProgress( percentage: percentage,
-                                   primaryColor: Colors.red, 
-                                   secondaryColor: Colors.blue,
-                                   strokePrimary: 5,
-                                   strokeSecondary: 10,)
-        // child: Text('${percentage}', style: TextStyle(fontSize: 50.0))),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          
+
+          Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                CustomRadialProgress(percentage: percentage, color: Colors.blue),
+                CustomRadialProgress(percentage: percentage, color: Colors.red),
+              ],
           ),
-        // child: CircularProgress(50, Color, ),
-      ),
+
+          Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                CustomRadialProgress(percentage: percentage, color: Colors.purple),
+                CustomRadialProgress(percentage: percentage, color: Colors.pink),
+              ],
+          ),
+
+        ],
+      ) 
+    );
+  }
+}
+
+class CustomRadialProgress extends StatelessWidget {
+
+  final Color color;
+
+  const CustomRadialProgress({ 
+    required this.percentage,
+    required this.color
+  })  ;
+
+  final double percentage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150,
+      height: 150,
+      //color: Colors.red,
+      child: RadialProgress( percentage: percentage,
+                             primaryColor: color, 
+                             secondaryColor: Colors.blue,
+                             strokePrimary: 5,
+                             strokeSecondary: 10,)
+        // child: Text('${percentage}', style: TextStyle(fontSize: 50.0))),
     );
   }
 }
