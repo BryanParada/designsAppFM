6f1ef4142962eed37748fbfeb9f1ab70e4b695f4
Bryan Parada <bryan.parada.cea@gmail.com>
2023-01-17 14:40:16 -0300

    FM65 - Provider install

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@
 //import 'package:designs_backgrounds/src/pages/headers_page.dart';
 //import 'package:designs_backgrounds/src/labs/circular_progress_page.dart';
 //import 'package:designs_backgrounds/src/pages/graphs_circular_page.dart';
-import 'package:designs_backgrounds/src/pages/slideshow.dart';
+import 'package:designs_backgrounds/src/pages/slideshow_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
