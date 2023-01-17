07b722cd7fa8c731ec26ab263af78e135f5459ce
Bryan Parada <bryan.parada.cea@gmail.com>
2023-01-17 20:17:47 -0300

    FM83 - Menu Design

--- lib/src/widgets/pinterest_menu.dart
+++ lib/src/widgets/pinterest_menu.dart
@@ -23,8 +23,55 @@ class PinterestMenu extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        child: Text('hola'),
+
+        child: _MenuItems( items),
+
+        width: 250,
+        height: 60,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(100)),
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+              color: Colors.black38,
+              // offset: Offset(5,5),
+              blurRadius: 10,
+              spreadRadius: -5
+            )
+          ]
+        ),
       ),
+    );
+  }
+}
+
+class _MenuItems extends StatelessWidget {
+  final List<PinterestButton> menuItems;
+
+  const _MenuItems( 
+   this.menuItems);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: List.generate(menuItems.length, (i) => _PinterestMenuButton(i, menuItems[i])),
+    );
+  }
+}
+
+
+class _PinterestMenuButton extends StatelessWidget {
+
+  final int index;
+  final PinterestButton item;
+
+  const _PinterestMenuButton(this.index, this.item);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Icon(item.icon)
     );
   }
 }
