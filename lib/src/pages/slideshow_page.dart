7c8f920e57a068bbf7d0125fbd3246caec0f46eb
Bryan Parada <bryan.parada.cea@gmail.com>
2023-01-17 15:01:18 -0300

    FM67 - Improve dots animation

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -57,12 +57,14 @@ class _Dot extends StatelessWidget {
 
     final pageViewIndex = Provider.of<SliderModel>(context).currentPage;
 
-    return Container(
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 200),
       width: 12,
       height: 12,
       margin: EdgeInsets.symmetric(horizontal: 5),
       decoration: BoxDecoration(
-        color: (pageViewIndex == index) ? Colors.blue : Colors.grey,
+        color: (pageViewIndex >= index - 0.5 && pageViewIndex < index + 0.5)
+         ? Colors.blue : Colors.grey,
         shape: BoxShape.circle
 
       ),
