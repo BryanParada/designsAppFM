0e7d92e73449bf78d1376e7ce3153902c2e51194
Bryan Parada <bryan.parada.cea@gmail.com>
2023-01-19 18:01:27 -0300

    FM121 - Basic animations - animate_do

--- lib/src/pages/animate_do_page.dart
+++ lib/src/pages/animate_do_page.dart
@@ -1,3 +1,5 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -9,7 +11,10 @@ class AnimatePage1 extends StatelessWidget {
     return Scaffold(
       
         appBar: AppBar(
-          title: Text('Animate_do'),
+          title: FadeIn(
+            duration: Duration(milliseconds: 500),
+            child: Text('Animate_do')
+            ),
           actions: <Widget>[
 
             IconButton(
@@ -17,33 +22,54 @@ class AnimatePage1 extends StatelessWidget {
               onPressed: (){},
             ),
 
-            IconButton(
-              icon: Icon(Icons.navigate_next),
-              onPressed: (){},
+            SlideInLeft(
+              from: 50,
+              child: IconButton(
+                icon: Icon(Icons.navigate_next),
+                onPressed: (){
+                  Navigator.push(
+                    context, CupertinoPageRoute(builder: (_) => AnimatePage1())
+                    );
+                },
+              ),
             ),
           ],
           ),
 
-            floatingActionButton: FloatingActionButton(
-              child: FaIcon(FontAwesomeIcons.play),
-              onPressed: (){}
-              ),
+            floatingActionButton: ElasticInRight(
+              child: FloatingActionButton(
+                child: FaIcon(FontAwesomeIcons.play),
+                onPressed: (){}
+                ),
+            ),
 
             body: Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
 
-                  Icon(Icons.new_releases, color: Colors.blue, size: 40),
+                  ElasticIn(
+                    delay: Duration(milliseconds: 1100),
+                    child: Icon(Icons.new_releases, color: Colors.blue, size: 40)
+                    ),
 
-                  Text('Title', style: TextStyle(fontSize: 40, fontWeight: FontWeight.w200)),
+                  FadeInDown(
+                    delay: Duration(milliseconds: 200),
+                    child: Text('Title', style: TextStyle(fontSize: 40, fontWeight: FontWeight.w200))
+                    ),
 
-                  Text('I am a little text', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w200)),
+                  FadeInDown(
+                    delay: Duration(milliseconds: 800),
+                    child: Text('I am a little text', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w200))
+                    ),
 
-                  Container(
-                    width: 220,
-                    height: 2,
-                    color: Colors.blue
+                  FadeInLeft(
+                    delay: Duration(milliseconds: 1100),
+                    child: Container(
+                      width: 220,
+                      height: 2,
+                      color: Colors.blue
+                    ),
                   )
 
                 ],
